2fa310ff64e5ed8d912b08f96c2667455a3918c1
RodrigoDigo29 <75053598+RodrigoDigo29@users.noreply.github.com>
2021-01-02 22:05:41 -0300

    criaçao de um script

---
+++ Assets/scripts/PlayerMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    private bool facingRight = true;
+
+    //variaveis para joystick
+    private float horizontalMove = 0f;
+    public Joystick joystick;
+    public float runSpeedHorizontal = 2;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        FixedUpdate();
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        float moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } else if(facingRight && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
