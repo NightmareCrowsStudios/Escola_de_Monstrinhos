798370058fe9e7fa676cc1e25867c31cecbec39c
RodrigoDigo29 <75053598+RodrigoDigo29@users.noreply.github.com>
2021-01-04 22:43:31 -0300

    configurando cenario

--- Assets/scripts/PlayerMove.cs
+++ Assets/scripts/PlayerMove.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMove : MonoBehaviour
 {
     public float speed;
     public float jumpForce;
+    public float runSpeed;
     private Rigidbody2D rb;
     private bool facingRight = true;
 
@@ -13,6 +15,8 @@ public class PlayerMove : MonoBehaviour
     private float horizontalMove = 0f;
     public Joystick joystick;
     public float runSpeedHorizontal = 2f;
+
+    public Animator anim;
 
     void Start()
     {
@@ -28,16 +32,24 @@ public class PlayerMove : MonoBehaviour
         horizontalMove = joystick.Horizontal * runSpeedHorizontal;
         transform.position += new Vector3(horizontalMove, 0 , 0) * Time.deltaTime * speed;
 
+        if(horizontalMove < 0)
+        {
+            anim.Play("IdleEsquerda");
+        } else
+        {
+            anim.Play("Idle");
+        }
+
         //float moveInput = Input.GetAxis("Horizontal");
         //rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if(facingRight == false && horizontalMove > 0)
+        /*if(facingRight == false && horizontalMove > 0)
         {
             Flip();
         } else if(facingRight && horizontalMove < 0)
         {
             Flip();
-        }
+        }*/
 
         /*if(horizontalMove > 0)
         {
@@ -47,6 +59,15 @@ public class PlayerMove : MonoBehaviour
         {
             transform.eulerAngles = new Vector3(0f,180f,0f); //olhando para esquerda
         }*/
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+        if(transform.position.y < -5.30f)
+        {
+            SceneManager.LoadScene(2);
+        }
     }
 
     void Flip()
@@ -63,6 +84,14 @@ public class PlayerMove : MonoBehaviour
         {
             rb.velocity = Vector2.up * jumpForce;
             //rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    public void Run()
+    {
+        if(rb.velocity.y == 0)
+        {
+            transform.position += new Vector3(horizontalMove, 0 , 0) * Time.deltaTime * runSpeed;
         }
     }
 }
